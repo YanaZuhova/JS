697fb937bd97fc4d67ee969c08db091e867eabc5
YanaZuhova <ziukhovaiana@gmail.com>
2023-08-14 13:33:40 +0300

    DZ.3.5

---
+++ mail.js
@@ -0,0 +1,27 @@
+let str = "Hello, World";
+let strNumber = "5";
+let emptyStr = "";
+let number = 47;
+let bool = false;
+
+console.log(Number(str));
+console.log(Number(strNumber));
+console.log(Number(emptyStr));
+console.log(Number(number));
+console.log(Number(bool));
+
+console.log(String("---"))
+
+console.log(String(str));
+console.log(String(strNumber));
+console.log(String(emptyStr));
+console.log(String(number));
+console.log(String(bool));
+
+console.log(String("---"))
+
+console.log(Boolean(str));
+console.log(Boolean(strNumber));
+console.log(Boolean(emptyStr));
+console.log(Boolean(number));
+console.log(Boolean(bool));
